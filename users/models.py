182e412e5dc55e9b652e882a0dd35790b128c8c7
vandersoncurty <vandersoncurty.contato@proton.me>
2025-05-31 14:18:44 -0300

    added more fields

--- users/models.py
+++ users/models.py
@@ -6,8 +6,18 @@ class User(models.Model):
     """
     User model for the GAR application.
     """
-    username = models.CharField(max_length=150, unique=True)
-    password = models.CharField(max_length=128)
+    email = models.EmailField(unique=True)
+    password_hash = models.CharField(max_length=128)
     first_name = models.CharField(max_length=30, blank=True, null=True)
     last_name = models.CharField(max_length=30, blank=True, null=True)
-    role = models.ForeignKey('Role', on_delete=models.SET_NULL, null=True)
+    cpf = models.CharField(max_length=11, unique=True, blank=True, null=True)
+    rg = models.CharField(max_length=10, unique=True, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    profission = models.CharField(max_length=50, blank=True, null=True)
+    spouse = models.CharField(max_length=50, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    role_id = models.ForeignKey('Role', on_delete=models.SET_NULL, null=True, related_name='users')
+    address_id = models.ForeignKey('Address', on_delete=models.SET_NULL, null=True, related_name='users')
+    contact_id = models.ForeignKey('Contact', on_delete=models.SET_NULL, null=True, related_name='users')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
